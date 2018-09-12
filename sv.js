600db329f8a963da7fa2c1b51bcf4c700b14c5fe
Igor <40350524+thecobolt@users.noreply.github.com>
2018-09-12 14:57:28 +0300

    Update and rename src/index.js to sv.js

---
+++ sv.js
@@ -0,0 +1,28 @@
+class Sorter {
+    constructor() {
+        this.array = []; 
+        this.compareFunction = (left,right) => left - right; 
+    }
+    add(element) {
+        this.array.push(element); 
+    }
+    at(index) {
+        return this.array[index]; 
+    }
+    get length() {
+        return this.array.length; 
+    }
+    toArray() {
+        return this.array; /
+    }
+    sort(indices) {
+        var sortArray = [];
+        indices.sort().forEach(x => sortArray.push(this.array[x]));
+        sortArray.sort(this.compareFunction);  
+        indices.forEach((x, y) => this.array[x] = sortArray[y]);
+    }
+    setComparator(compareFunction) {
+        this.compareFunction = compareFunction;
+  }
+}
+module.exports = Sorter;
